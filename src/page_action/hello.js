43ddffef79e689aea76cc4f096eb4f0334413d5d
nampham148 <phamquangnam148@gmail.com>
2018-02-08 18:18:21 -0500

    Feb 8

--- src/page_action/hello.js
+++ src/page_action/hello.js
@@ -1,2 +1,23 @@
-var exists = document.getElementsByClassName("yt-simple-endpoint style-scope ytd-video-owner-renderer");
-console.log("WORKS?" + exists.getAttribute("class"));
+//alert("This is youtube");
+
+
+/*
+chrome.extension.sendMessage({instruction: "getYTid"}, function(response) {
+	//alert(response.url);
+	$.getJSON('https://noembed.com/embed', {format: 'json', url: response.url}, function (data) {
+  		if (data.author_name){
+  			history.pushState(null, null, response.url + "/channel=" + data.author_name + "/");
+  		}
+    	
+	});
+});
+*/
+
+$(document).ready(function () {
+  	$.getJSON('https://noembed.com/embed', {format: 'json', url: location.href}, function (data) {
+  		if (data.author_name){
+  			history.pushState(null, null, location.href + "/channel=" + data.author_name + "/");
+  		}
+    });
+});
+

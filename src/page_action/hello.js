e057bfa2d37a3493b062eccd1604ff892edf5a8a
nampham148 <phamquangnam148@gmail.com>
2018-03-20 22:31:22 -0400

    url sticks but needs to reload once to work

--- src/page_action/hello.js
+++ src/page_action/hello.js
@@ -1,12 +1,13 @@
 function changeURL(){
   $.getJSON('https://noembed.com/embed', {format: 'json', url: location.href}, function (data) {
         if (data.author_name){
-          history.replaceState(null, null, location.href + "&channel=" + data.author_name);
+          history.replaceState(null, null, "watch?v=" + YouTubeGetID(location.href) + "&channel=" + data.author_name);
         }
   });
 }
 
 changeURL();
+setTimeout(changeURL, 2000);
 
 document.addEventListener('transitionend', function(e) {
     if (e.target.id === 'progress'){
